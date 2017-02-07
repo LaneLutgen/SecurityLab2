d59bedac3f49616611982196ffd1b5c1a587de2d
Lane Lutgen <lutgenlane@gmail.com>
2017-02-06 18:39:45 -0700

    Fixed timer and set file input to be through command line

--- src/DictionaryAttack.java
+++ src/DictionaryAttack.java
@@ -12,22 +12,44 @@ public class DictionaryAttack {
 	public static void main(String[] args) {		
 		
 		double startTime;
-	    	double finishTime;
-        	double elapsedSeconds;
-        	startTime = System.currentTimeMillis();
+    	double finishTime;
+    	double elapsedSeconds;
 		BufferedReader buffer = null;
 		List<String> hashvalue = new ArrayList<String>();
 	 	String curLine = null;
 		
 		try {
-			buffer = new BufferedReader(new FileReader("/input/hashvalue.txt"));
+			String inputFilePath = null;
+			String dictionaryFilePath = null;
+			
+			if(args.length > 0)
+			{
+				inputFilePath = args[0];
+			}
+			else
+			{
+				//If no command args are provided, assume the file is in the same directory as the source
+				inputFilePath = "hashvalue.txt";
+			}
+			
+			if(args.length > 1)
+			{
+				dictionaryFilePath = args[1];
+			}
+			else
+			{
+				//If no command args are provided, assume the file is in the same directory as the source
+				dictionaryFilePath = "dictionary.txt";
+			}
+			
+			buffer = new BufferedReader(new FileReader(inputFilePath));
 			
 			while ((curLine = buffer.readLine()) != null) {
 				hashvalue.add(curLine);
 			}
 	   	
-			buffer = new BufferedReader(new FileReader("/input/"));		//need to add the dictionary filepath here
-		
+			buffer = new BufferedReader(new FileReader(dictionaryFilePath));		//need to add the dictionary filepath here
+			startTime = System.currentTimeMillis();
 			while ((curLine = buffer.readLine()) != null) {
 				int i = hashvalue.indexOf(encryptHashValue(curLine));
 			
@@ -37,12 +59,15 @@ public class DictionaryAttack {
 					System.out.println();	
 					System.out.print("The password for hash value " +hashvalue.get(i)+ " is " +curLine+ ",it takes the program " +elapsedSeconds+ " sec to recover this password");	
 					hashvalue.remove(i);
+					startTime = System.currentTimeMillis();
 				}
 			}
 		}
 		
 		catch(IOException e) {
 			//Handle exception 
+			System.err.println("Error occured. Please enter the file paths as command line arguments");
+			System.err.println("args[0] = Input File Path, args[1] = Dictionary File Path");
 		}
 	}
 	
